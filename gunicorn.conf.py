0fa2b256ff7a184b21aa635410d805a22adb982e
Amaza-Ronald <amazaronaldo@gmail.com>
2025-12-02 22:31:19 +0300

    New changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,10 @@
 # gunicorn.conf.py
 bind = "0.0.0.0:10000"
-workers = 2
-worker_class = "gevent"
+workers = 1
+threads = 2
+worker_class = "gthread"
 timeout = 120
-keepalive = 2
+keepalive = 5
 max_requests = 1000
-max_requests_jitter = 100
-preload_app = True
+max_requests_jitter = 50
+preload_app = False
